cd03c398be508da011d9cdffdbcc6d5ec61679eb
ShreyasSatewar <shreyas.satewar@gmail.com>
2020-10-24 09:32:01 -0500

    second commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -10,5 +10,6 @@ def say_hello_py(x):
 say_hello_py('Python World!')
 eel.say_hello_js('Python World!')   # Call a Javascript function
 
-eel.start('hello.html', size=(300, 200))  # Start
+eel.start('index.html', size=(300, 200))  # Start
+
 
