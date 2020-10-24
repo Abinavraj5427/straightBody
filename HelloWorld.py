be2303004434043c5fd85b4ae076626684ce3264
ShreyasSatewar <shreyas.satewar@gmail.com>
2020-10-23 23:42:58 -0500

    second commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import eel
+
+# Set web files folder
+eel.init('webpage')
+
+@eel.expose                         # Expose this function to Javascript
+def say_hello_py(x):
+    print('Hello from %s' % x)
+
+say_hello_py('Python World!')
+eel.say_hello_js('Python World!')   # Call a Javascript function
+
+eel.start('hello.html', size=(300, 200))  # Start
+
