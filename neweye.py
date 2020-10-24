4692d58f5802cdb369acf4709e5b3db211cbbc02
Daveraj Chakravarty <drajchakravarty@hotmail.com>
2020-10-24 16:38:49 -0500

    Add on to the blink detector

--- neweye.py
+++ neweye.py
@@ -8,7 +8,7 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(r"C:/Users/drajc/OneDrive/Desktop/straightBody/shape_predictor_68_face_landmarks.dat")
 
 def midpoint(p1, p2):
-    return int(p1.x + p2.x)/2, int(p1.y + p2.y)/2
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
 while True:
     _, frame = cap.read()
@@ -24,11 +24,11 @@ while True:
         landmarks = predictor(gray, face)
         left_point = (landmarks.part(36).x, landmarks.part(36).y)
         right_point = (landmarks.part(39).x, landmarks.part(39).y)
-        center_top = midpoint(landmarks.part(37), lamdmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41), lamdmarks.part(40))
+        center_top = midpoint(landmarks.part(37), landmarks.part(38))
+        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
 
         hor_line = cv2.line(frame, left_point, right_point, (0,255,0),2)
-
+        ver_line = cv2.line(frame, center_top, center_bottom, (0,255,0), 2)
 
 
         #x =  landmarks.part(36).x
