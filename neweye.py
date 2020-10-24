a83eaba6aa0e17b4916b918b5696eb27a7ee2f84
Daveraj Chakravarty <drajchakravarty@hotmail.com>
2020-10-24 18:01:41 -0500

    Finished blinking setup

--- neweye.py
+++ neweye.py
@@ -13,6 +13,23 @@ def midpoint(p1, p2):
 
 font = cv2.FONT_HERSHEY_COMPLEX
 
+def get_blinking_ratio(eye_point, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_point[0]).x, facial_landmarks.part(eye_point[0]).y)
+    right_point = (facial_landmarks.part(eye_point[3]).x, facial_landmarks.part(eye_point[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_point[1]), facial_landmarks.part(eye_point[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_point[5]), facial_landmarks.part(eye_point[4]))
+
+    hor_line = cv2.line(frame, left_point, right_point, (0,255,0),2)
+    ver_line = cv2.line(frame, center_top, center_bottom, (0,255,0), 2)
+
+    hor_line_length = hypot((left_point[0]-right_point[0]), (left_point[1]- right_point[1]))
+    ver_line_length = hypot((center_top[0]- center_bottom[0]), (center_top[1] - center_bottom[1]))
+        #print(str(ver_line_length))
+        #print(ver_line_length, hor_line_length)
+    ratio = (hor_line_length/ver_line_length)
+    return ratio
+    
+
 while True:
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -21,24 +38,24 @@ while True:
     for face in faces:
         x, y = face.left(), face.top()
         x1, y1 = face.right(), face.bottom()
-        #
-        # cv2.rectangle(frame, (x,y),(x1,y1),(0,255,0),2)
-
         landmarks = predictor(gray, face)
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
-        center_top = midpoint(landmarks.part(37), landmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+        # cv2.rectangle(frame, (x,y),(x1,y1),(0,255,0),2)
+        right_eye_ratio = get_blinking_ratio([36,37,38,39,40,41], landmarks)
+        left_eye_ratio = get_blinking_ratio([42,43,44,45,46,47], landmarks)
+        #left_point = (landmarks.part(36).x, landmarks.part(36).y)
+        #right_point = (landmarks.part(39).x, landmarks.part(39).y)
+        #center_top = midpoint(landmarks.part(37), landmarks.part(38))
+        #center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
 
-        hor_line = cv2.line(frame, left_point, right_point, (0,255,0),2)
-        ver_line = cv2.line(frame, center_top, center_bottom, (0,255,0), 2)
+        #hor_line = cv2.line(frame, left_point, right_point, (0,255,0),2)
+        #ver_line = cv2.line(frame, center_top, center_bottom, (0,255,0), 2)
 
-        hor_line_length = hypot((left_point[0]-right_point[0]), (left_point[1]- right_point[1]))
-        ver_line_length = hypot((center_top[0]- center_bottom[0]), (center_top[1] - center_bottom[1]))
+       # hor_line_length = hypot((left_point[0]-right_point[0]), (left_point[1]- right_point[1]))
+        #ver_line_length = hypot((center_top[0]- center_bottom[0]), (center_top[1] - center_bottom[1]))
         #print(str(ver_line_length))
         #print(ver_line_length, hor_line_length)
-        ratio = (hor_line_length/ver_line_length)
-        if ratio > 5.7:
+        #ratio = (hor_line_length/ver_line_length)
+        if left_eye_ratio > 5.7 and right_eye_ratio > 5.7:
             cv2.putText(frame, "Blinking", (50,150), font, 4, (255,0,0))
 
         #x =  landmarks.part(36).x
