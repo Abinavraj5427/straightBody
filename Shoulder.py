d348de1bcb03f7fab869ef95c94f9d4723212bc6
Abinavraj5427 <abinavraj10@gmail.com>
2020-10-24 22:16:35 -0500

    Added area for shoulder contours

--- Shoulder.py
+++ Shoulder.py
@@ -17,59 +17,65 @@ max_vertices = 18000
 min_vertices = 0
 min_ratio = 0
 max_ratio = 1000
-max_left_point = 0
-max_right_point = 0
+# max_left_point = 0
+# max_right_point = 0
 while True:
-    ret, imgorg = cap.read()
+    ret, raw = cap.read()
+    imgorg = raw.copy()
     frame = cv2. cvtColor(imgorg, cv2.COLOR_BGR2HSV)
     frame = cv2.inRange(frame, (hue[0], sat[0], val[0]), (hue[1], sat[1], val[1]))
     ksize = int(6 * round(5) + 1)
     frame = cv2.GaussianBlur(frame, (ksize, ksize), round(5))
     frame = cv2.normalize(frame, frame, 0, 255, cv2.NORM_MINMAX)
-    _, threshold = cv2.threshold(frame, 12, 255, cv2.THRESH_BINARY_INV)
+    _, threshold = cv2.threshold(frame, 5, 255, cv2.THRESH_BINARY_INV)
 
-    contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    #print(len(contours))
-    for contour in contours:
-        for i in range(len(contour)):
-            print(contour[i][0])
-            #if (max_left_point < contour[i][0]):
-                #max_left_point = contour[i][0]
+    contours, _ = cv2.findContours(frame, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    input_contours = contours
+    output = []
+    for contour in input_contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        if (w < min_width or w > max_width):
+            continue
+        if (h < min_height or h > max_height):
+            continue
+        area = cv2.contourArea(contour)
+        if (area < min_area):
+            continue
+        if (cv2.arcLength(contour, True) < min_perimeter):
+            continue
+        hull = cv2.convexHull(contour)
+        solid = 100 * area / cv2.contourArea(hull)
+        if (solid < solidity[0] or solid > solidity[1]):
+            continue
+        if (len(contour) < min_vertices or len(contour) > max_vertices):
+            continue
+        ratio = (float)(w) / h
+        if (ratio < min_ratio or ratio > max_ratio):
+            continue
+        output.append(contour)
+    contournew = output
+
+    contour = cv2.drawContours(imgorg, contournew, -1, (0, 0, 255), 3)
+
+    # GETS THE MAXIMUM LENGTH CONTOUR
+    max_contour = 0
+    max_contour_index = 0
+    for i in range(len(contournew)):
+        if max_contour < len(contournew[i]):
+            max_contour_index = i
 
 
-            print(contour)
+    largest_contour = contournew[max_contour_index]
+    area = cv2.contourArea(largest_contour)
+    print(area)
+    # for i in range(len(largest_contour)):
+        # print(contour[i][0])
+        # if (max_left_point < contour[i][0]):
+        #     max_left_point = contour[i][0]
 
-    #input_contours = contours
-    #output = []
-    # for contour in input_contours:
-    #     x, y, w, h = cv2.boundingRect(contour)
-    #     if (w < min_width or w > max_width):
-    #         continue
-    #     if (h < min_height or h > max_height):
-    #         continue
-    #     area = cv2.contourArea(contour)
-    #     if (area < min_area):
-    #         continue
-    #     if (cv2.arcLength(contour, True) < min_perimeter):
-    #         continue
-    #     hull = cv2.convexHull(contour)
-    #     solid = 100 * area / cv2.contourArea(hull)
-    #     if (solid < solidity[0] or solid > solidity[1]):
-    #         continue
-    #     if (len(contour) < min_vertices or len(contour) > max_vertices):
-    #         continue
-    #     ratio = (float)(w) / h
-    #     if (ratio < min_ratio or ratio > max_ratio):
-    #         continue
-    #     output.append(contour)
-    # contournew = output
-    #
-    # print(contournew)
 
-    #print("SHAPE")
-    #print(imgorg.shape)
-    contour = cv2.drawContours(imgorg, contours, -1, (0, 0, 255), 3)
-
+    #print(contour)
 
 
     cv2.imshow("Frame", frame)
