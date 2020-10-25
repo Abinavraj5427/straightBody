1d4c29989a307e82637b4ed103191f23f610df95
ShreyasSatewar <shreyas.satewar@gmail.com>
2020-10-25 08:49:28 -0500

    live updates for eye and posture

--- webpage/js/analytics.js
+++ webpage/js/analytics.js
@@ -1,3 +1,4 @@
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
@@ -16,57 +17,93 @@ var firestore = firebase.firestore();
 
 var user = "Shreyas";
 var docRef = firestore.collection("Users").doc(user);
-var userID;
-window.onload = function fetch() {
-    docRef.get().then(function (doc) {
-        if (doc && doc.exists) {
-            const myData = doc.data();
-            document.getElementById("titleName").innerHTML = myData.FirstName;
-            userID = myData.UserNumber;
-        }
-        else {
-            console.log("error");
-        }
-    })
-}
+// window.onload = function fetch() {
+//     docRef.get().then(function (doc) {
+//         if (doc && doc.exists) {
+//             // console.log("test");
+//             const myData = doc.data();
+//             //document.getElementById("titleName").innerHTML = myData.FirstName;
+//             userID = myData.UserNumber;
+//         }
+//         else {
+//             console.log("error");
+//         }
+//     })
 
-var data2;
-function load() {
-    docRef = firestore.collection("EyeMovement").doc(String(userID));
-    docRef.onSnapshot(function (doc) {
-        const myData = doc.data();
-        data2 = myData.FocusedTime;
-        console.log(data2)
+// }
+
+window.onload = function load() {
+    var keys = [];
+    var values = [];
+
+    const events = firebase.firestore().collection('Postures')
+    events.onSnapshot((querySnapshot) => {
+        const tempDoc = []
+        querySnapshot.forEach((doc) => {
+            keys.push(doc.data().time);
+            values.push(doc.data().area);
+            tempDoc.push({ id: doc.id, ...doc.data() })
+        })
+        console.log(tempDoc)
         update();
     })
 
-    function update(){
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var keys = [];
-    var values = [];
-    for (i = 0; i < data2.length; i++) {
-        keys.push(i);
-        values.push(data2[i]);
-
+    function update() {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+            // The data for our dataset
+            data: {
+                labels: keys,
+                datasets: [{
+                    label: 'Posture',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: values,
+                }]
+            },
+            // Configuration options go here
+            options: {}
+        });
     }
-    var arr = [0, 1, 2]
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-        // The data for our dataset
-        data: {
-            labels: keys,
-            datasets: [{
-                label: 'Posture',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: values,
-            }]
-        },
-        // Configuration options go here
-        options: {}
-    });
-}
+
+    const events2 = firebase.firestore().collection('EyeMovement')
+    events2.onSnapshot((querySnapshot) => {
+        const tempDoc = []
+        querySnapshot.forEach((doc) => {
+            keys.push(doc.data().time);
+            if (doc.data().onScreen == false){
+                values.push(0);
+            }
+            else{
+                values.push(1);
+            }
+            tempDoc.push({ id: doc.id, ...doc.data() })
+        })
+        console.log(tempDoc)
+        update2();
+    })
+
+    function update2() {
+        var ctx = document.getElementById('otherChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+            // The data for our dataset
+            data: {
+                labels: keys,
+                datasets: [{
+                    label: 'Eye Movement',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: values,
+                }]
+            },
+            // Configuration options go here
+            options: {}
+        });
+    }
 }
 
 
