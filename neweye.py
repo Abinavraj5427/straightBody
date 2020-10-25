d8cf594309dfe769df29db6b438f31888f072aed
Daveraj Chakravarty <drajchakravarty@hotmail.com>
2020-10-25 00:10:54 -0500

    Two ways

--- neweye.py
+++ neweye.py
@@ -29,8 +29,42 @@ def get_blinking_ratio(eye_point, facial_landmarks):
         #print(ver_line_length, hor_line_length)
     ratio = (hor_line_length/ver_line_length)
     return ratio
-    
 
+def get_gaze_ratio(eye_points, facial_landmarks):
+    left_eye_region = np.array([(facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y),
+                                (facial_landmarks.part(eye_points[1]).x, facial_landmarks.part(eye_points[1]).y),
+                                (facial_landmarks.part(eye_points[2]).x, facial_landmarks.part(eye_points[2]).y),
+                                (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y),
+                                (facial_landmarks.part(eye_points[4]).x, facial_landmarks.part(eye_points[4]).y),
+                                (facial_landmarks.part(eye_points[5]).x, facial_landmarks.part(eye_points[5]).y)], np.int32)
+        #print(left_eye_region)
+        #cv2.polylines(frame, [left_eye_region], True, (0,0,255),2)
+        
+    height, width, _ = frame.shape
+    mask = np.zeros((height,width), np.uint8)
+    cv2.polylines(mask, [left_eye_region], True, 255,2)
+    cv2.fillPoly(mask, [left_eye_region], 255)
+    left_eye = cv2.bitwise_and(gray, gray, mask=mask)
+
+    min_x = np.min(left_eye_region[:, 0])
+    max_x = np.max(left_eye_region[:, 0])
+    min_y = np.min(left_eye_region[:, 1])
+    max_y = np.max(left_eye_region[:, 1])
+
+    gray_eye = left_eye[min_y:max_y, min_x: max_x]
+        #gray_eye = cv2.cvtColor(eye, cv2.COLOR_BGR2GRAY)
+    _, threshold_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY)
+    height, width = threshold_eye.shape
+    left_side_threshold = threshold_eye[0: height, 0: int(width/2)]
+    left_side_white = cv2.countNonZero(left_side_threshold)
+
+    right_side_threshold = threshold_eye[0: height, int(width/2):width]
+    right_side_white = cv2.countNonZero(right_side_threshold)   
+    if right_side_white == 0:
+        return 0
+    else:
+        gaze_ratio = left_side_white/right_side_white
+        return gaze_ratio
 while True:
     time_counter +=1
     _, frame = cap.read()
@@ -51,48 +85,26 @@ while True:
             cv2.putText(frame, "Blinking", (50,150), font, 4, (255,0,0))
 
         #GAZE DECTECTION CODE
-        left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
-                                    (landmarks.part(37).x, landmarks.part(37).y),
-                                    (landmarks.part(38).x, landmarks.part(38).y),
-                                    (landmarks.part(39).x, landmarks.part(39).y),
-                                    (landmarks.part(40).x, landmarks.part(40).y),
-                                    (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
-        #print(left_eye_region)
-        #cv2.polylines(frame, [left_eye_region], True, (0,0,255),2)
         
-        height, width, _ = frame.shape
-        mask = np.zeros((height,width), np.uint8)
-        cv2.polylines(mask, [left_eye_region], True, 255,2)
-        cv2.fillPoly(mask, [left_eye_region], 255)
-        left_eye = cv2.bitwise_and(gray, gray, mask=mask)
+        #if right_side_white != 0:
+            #gaze_ratio = left_side_white/right_side_white
+        #if (right_side_white < 54 and right_side_white >= 5) and (left_side_white < 87 and left_side_white >= 35):
+            #counter+=1
+       # cv2.putText(frame, str(left_side_white), (50,100), font, 2, (0,0,255),3)
+        #cv2.putText(frame, str(right_side_white), (50,150), font, 2, (0,0,255),3)
+        
 
-        min_x = np.min(left_eye_region[:, 0])
-        max_x = np.max(left_eye_region[:, 0])
-        min_y = np.min(left_eye_region[:, 1])
-        max_y = np.max(left_eye_region[:, 1])
 
-        gray_eye = left_eye[min_y:max_y, min_x: max_x]
-        #gray_eye = cv2.cvtColor(eye, cv2.COLOR_BGR2GRAY)
-        _, threshold_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY)
-        height, width = threshold_eye.shape
-        left_side_threshold = threshold_eye[0: height, 0: int(width/2)]
-        left_side_white = cv2.countNonZero(left_side_threshold)
-
-        right_side_threshold = threshold_eye[0: height, int(width/2):width]
-        right_side_white = cv2.countNonZero(right_side_threshold)
-        if (right_side_white < 54 and right_side_white >= 5) and (left_side_white < 85 and left_side_white >= 35):
-            counter+=1
-        cv2.putText(frame, str(left_side_white), (50,100), font, 2, (0,0,255),3)
-        cv2.putText(frame, str(right_side_white), (50,150), font, 2, (0,0,255),3)
-
-        eye = cv2.resize(gray_eye, None, fx=6, fy=6)
-        threshold_eye = cv2.resize(threshold_eye, None, fx=6, fy=6)
+       
 
         #cv2.imshow("Eye", eye)
-        cv2.imshow("Threshold", threshold_eye)
-        cv2.imshow("left", left_side_threshold)
-        cv2.imshow("right", right_side_threshold)
-        cv2.imshow("Right Eye", left_eye)
+        gaze_ratio_left_eye= get_gaze_ratio([36,37,38,39,40,41], landmarks)
+        gaze_ratio_right_eye = get_gaze_ratio([42,43,44,45,46,47], landmarks)
+        gaze_ratio = (gaze_ratio_right_eye+gaze_ratio_left_eye)/2
+        #cv2.putText(frame, str(gaze_ratio_left_eye), (50,100), font, 2, (0,0,255),3)
+        cv2.putText(frame, str(gaze_ratio), (50,150), font, 2, (0,0,255),3)
+        if gaze_ratio >= .85 and gaze_ratio <= 2.2:
+            counter+=1
         #x =  landmarks.part(36).x
         #y = landmarks.part(36).y
         #cv2.circle(frame, (x,y), 3, (0,0,255),2)
