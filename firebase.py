c7135e030fa4543e59de71b362bf3ece06b5946a
Abinavraj5427 <abinavraj10@gmail.com>
2020-10-24 23:04:42 -0500

    Added firebase service account

--- firebase.py
+++ firebase.py
@@ -3,17 +3,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use the application default credentials
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-    apiKey: "AIzaSyBzkhAV5Of1Nc2MYaBe9PIYL_MmuSq5URw",
-    authDomain: "straightbody-4dec9.firebaseapp.com",
-    databaseURL: "https://straightbody-4dec9.firebaseio.com/",
-    projectId: "straightbody-4dec9",
-    storageBucket: "straightbody-4dec9.appspot.com",
-    messagingSenderId: "922824382292",
-    appId: "1:922824382292:web:69586bb5eac68bf0c51ac1",
-    measurementId: "G-C7RY3SJSX3"
-})
+def get_db():
+    # Use a service account
+    cred = credentials.Certificate('./serviceAccount/straightbody-4dec9-firebase-adminsdk-nawot-ce763cf334.json')
+    firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+    db = firestore.client()
+    return db
